e0d204086a70641922b829f87a1edbc09337a20e
Anton Nikolov <58844584+ddevil345@users.noreply.github.com>
2020-02-25 17:11:18 +0200

    more...

---
+++ Programming Fundamentals Mid Exam/Mid Exam - 2 November 2019/01. Biscuits Factory/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _01._Biscuits_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cookiesPerWorker = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+            int rival = int.Parse(Console.ReadLine());
+            
+            int total = 0;
+            
+            int lessDay = 0;
+            for (int i = 1; i <= 30; i++)
+            {
+                lessDay++;
+                double cookiesPerDay = cookiesPerWorker * workers;
+                if (lessDay == 3)
+                {
+                    cookiesPerDay = Math.Ceiling(cookiesPerDay * 0.75);
+                    lessDay = 0;
+                }
+                
+                total += (int)Math.Ceiling(cookiesPerDay);
+            }
+
+            if (total > rival)
+            {
+                
+                int diff = total - rival;
+                double result = (diff / (double)rival) * 100;
+                Console.WriteLine($"You have produced {total} biscuits for the past month.");
+                Console.WriteLine($"You produce {result:f2} percent more biscuits.");
+            }
+            else
+            {
+                int diff = rival - total;
+                double result = (diff / (double)rival) * 100;
+                Console.WriteLine($"You have produced {total} biscuits for the past month.");
+                Console.WriteLine($"You produce {result:f2} percent less biscuits.");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals Mid Exam/Mid Exam - 2 November 2019/02. Weaponsmith/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Weaponsmith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weapons = Console.ReadLine().Split("|").ToList();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "Done")
+                {
+                    break;
+                }
+                var arr = input.Split().ToList();
+
+                switch (arr[0])
+                {
+                    case "Move":
+                        if (arr[1] == "Left")
+                        {
+                            int index1 = int.Parse(arr[2]);
+                            if (index1 >= 1 && index1 < weapons.Count)
+                            {
+                                int index2 = index1 - 1;
+                                string leftItem = weapons[index2];
+                                string rirghtItem = weapons[index1];
+                                
+                                weapons.RemoveAt(index1);
+                                weapons.RemoveAt(index2);
+                                
+                                weapons.Insert(index2, rirghtItem);
+                                weapons.Insert(index1, leftItem);
+                                
+                                
+                            }
+                        }
+                        else if (arr[1] == "Right")
+                        {
+                            int index1 = int.Parse(arr[2]);
+                            if (index1 >= 0 && index1 < weapons.Count -2)
+                            {
+                                int index2 = index1 + 1;
+                                string leftItem = weapons[index2];
+                                string rirghtItem = weapons[index1];
+                                weapons.RemoveAt(index2);
+                                weapons.RemoveAt(index1);
+                                weapons.Insert(index1, leftItem);
+                                weapons.Insert(index2, rirghtItem);
+                                
+                            }
+
+                        }
+                        break;
+                    case "Check":
+                        if (arr[1] == "Even")
+                        {
+                            List<string> evenList = new List<string>();
+                            for (int i = 0; i < weapons.Count; i++)
+                            {
+                                if (i % 2 == 0)
+                                {
+                                    evenList.Add(weapons[i]);
+                                    
+                                }
+                            }
+                            Console.WriteLine(string.Join(" ",evenList));
+                        }
+                        else if (arr[1] == "Odd")
+                        {
+                            List<string> oddList = new List<string>();
+                            for (int i = 0; i < weapons.Count; i++)
+                            {
+                                if (i % 2 != 0)
+                                {
+                                    oddList.Add(weapons[i]);
+
+                                }
+
+                            }
+                            Console.WriteLine(string.Join(" ", oddList));
+                        }
+                        
+                        break;
+                }
+            }
+            
+            Console.WriteLine($"You crafted {string.Join("",weapons)}!");
+        }
+    }
+}
